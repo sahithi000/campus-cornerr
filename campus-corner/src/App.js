8196de3088228ab38df955c0d2be135b55dc2e0d
Sahithi <ghantasahithi251@gmail.com>
2024-04-25 21:56:48 +0530

    setup

--- campus-corner/src/App.js
+++ campus-corner/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router,Route,Switch} from 'react-router-dom' ;
+import home from './components/home';
+import login from './components/login';
+import signup from './components/signup';
+import { BrowserRouter as Router,Route,Switch,Routes} from 'react-router-dom' ;
+import { useState } from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav'>
+        <Router>
+          <Routes>
+            <Route path='/' element="{<login/>} "></Route>
+            <Route path='/signup' element="{<signup/>} "></Route>
+            <Route path='/home' element="{<home/>} "></Route>
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
